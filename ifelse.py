dd341d917a6031117165c8831874321a57cda299
shahir2520 <2520shahir@gmail.com>
2023-07-08 22:57:05 +0530

    ifelse

---
+++ ifelse.py
@@ -0,0 +1,19 @@
+a = 33
+b = 33
+if b > a: #false
+    print("b is greater than a")
+elif a == b: #pass another condition
+    print("a and b are equal")
+elif a > b:
+    print("a is greater")
+else:
+    print("all above conditions are false..")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:#no any condition in else part
+  print("a is greater than b")

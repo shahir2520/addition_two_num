bbbf816f6dbad20e620e7852edcbe39b4b956fcc
shahir2520 <2520shahir@gmail.com>
2023-07-08 23:02:38 +0530

    continue

---
+++ conti.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry",10,20.3,11]
+for x in fruits:
+  if x == "cherry":
+    continue
+  print(x)
+#skip the true condition & continue with next one.
+
+for i in range(211,215):
+  print(i)

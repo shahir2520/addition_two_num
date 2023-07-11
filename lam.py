0d22634eb7bb2bfc0efced0cabb7387bf2a7fabb
shahir2520 <2520shahir@gmail.com>
2023-07-11 19:46:19 +0530

    mesaage

--- lam.py
+++ lam.py
@@ -8,3 +8,11 @@ print(z)
 
 li=[1,2,3,4,5,6,7,8,9]
 print(tuple(filter(lambda x:x % 2==0,li)))
+#map method
+print(tuple(map(lambda x: x*2,li)))
+from functools import reduce
+lis=[200,300,400]
+z=reduce(lambda x,y: x+y,lis)
+print(z)
+
+
